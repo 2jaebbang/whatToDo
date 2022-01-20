e9536e857aefac2d3643ff704698b86fe95ebe33
2jaebbang <2jaebbang@naver.com>
2022-01-20 16:29:02 +0900

    feat: 라우터 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "@pages/Home";
+import CampDetail from "@pages/CampDetail";
+import CampApply from "@pages/CampApply";
+import Community from "@pages/Community";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Plant
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/detail" element={<CampDetail />}></Route>
+          <Route path="/apply" element={<CampApply />}></Route>
+          <Route path="/community" element={<Community />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
