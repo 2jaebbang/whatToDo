d642354c018fc3f14ed44eb8f2f63201cf667172
2jaebbang <2jaebbang@naver.com>
2022-01-21 02:23:54 +0900

    chore: eslint 설정 변경

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,12 @@ import Home from "@pages/Home";
 import CampDetail from "@pages/CampDetail";
 import CampApply from "@pages/CampApply";
 import Community from "@pages/Community";
-
+import Nav from "@components/Nav";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Nav />
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/detail" element={<CampDetail />}></Route>
