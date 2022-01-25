447b5f3bbf1ec504aa917334eea63a095748cc8b
2jaebbang <2jaebbang@naver.com>
2022-01-25 23:50:00 +0900

    feat: home campSection ui 추가

---
+++ src/components/index.tsx
@@ -0,0 +1,4 @@
+export { default as Nav } from "./Nav";
+export { default as CampCard } from "./CampCard";
+export { default as CommunityCard } from "./CommunityCard";
+export { default as Footer } from "./Footer";
