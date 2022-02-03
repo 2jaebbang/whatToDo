0f272b8188c89a74601737aee6ba2b18205ae418
2jaebbang <2jaebbang@naver.com>
2022-02-03 13:47:54 +0900

    feat: 스켈레톤 컴포넌트 UI 추가

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { CampType, ICamp, ICommunity } from "types/type";
-import { Nav, Footer, FooterM } from "components/index";
+import { Nav, Footer, FooterM, CardSectionSkeleton } from "components/index";
 import { Header } from "pages/Home/Header";
 import Banner from "pages/Home/Banner";
 import sqlImage from "resources/images/sqlImage.png";
@@ -13,9 +13,9 @@ import { getCampByType } from "apis/campApi";
 import { getCommunities } from "apis/communityApi";
 
 export default function Home() {
-  const [popularCamps, setPopularCamps] = useState<ICamp[]>([]);
-  const [saleCamps, setSaleCamps] = useState<ICamp[]>([]);
-  const [communities, setCommunites] = useState<ICommunity[]>([]);
+  const [popularCamps, setPopularCamps] = useState<ICamp[]>();
+  const [saleCamps, setSaleCamps] = useState<ICamp[]>();
+  const [communities, setCommunites] = useState<ICommunity[]>();
   const isMobile = useMediaQuery({ query: "(max-width: 480px)" });
   useEffect(() => {
     fetchCamps("popular");
@@ -35,24 +35,36 @@ export default function Home() {
     <Container>
       <Nav />
       <Header />
-      <CampSection
-        title="인기 부트 캠프"
-        camps={popularCamps}
-        isHeadField={false}
-      />
-      <CampSection
-        title="특가 할인 캠프"
-        camps={saleCamps}
-        isHeadField={true}
-      />
+      {popularCamps ? (
+        <CampSection
+          title="인기 부트 캠프"
+          camps={popularCamps}
+          isHeadField={false}
+        />
+      ) : (
+        <CardSectionSkeleton />
+      )}
+      {saleCamps ? (
+        <CampSection
+          title="특가 할인 캠프"
+          camps={saleCamps}
+          isHeadField={true}
+        />
+      ) : (
+        <CardSectionSkeleton />
+      )}
       <Banner />
       {!isMobile ? (
         <>
-          <CommunitySection
-            title="커뮤니티"
-            communities={communities}
-            isTopView={true}
-          />
+          {communities ? (
+            <CommunitySection
+              title="커뮤니티"
+              communities={communities}
+              isTopView={true}
+            />
+          ) : (
+            <CardSectionSkeleton />
+          )}
           <Footer />
         </>
       ) : (
