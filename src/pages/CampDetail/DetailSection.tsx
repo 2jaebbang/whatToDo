34c59877aa74179f147c0be1472c36eb78bbe2fe
2jaebbang <2jaebbang@naver.com>
2022-02-08 16:48:47 +0900

    fix: 상세페이지 공통 컨테이너 수정

--- src/pages/CampDetail/DetailSection.tsx
+++ src/pages/CampDetail/DetailSection.tsx
@@ -1,31 +1,18 @@
-import DetailBanner from "components/DetailBanner";
-import { CommunityCard } from "components/index";
 import styled from "styled-components";
-import fonts from "styles/fonts";
-import { maxWidth } from "styles/mixin";
+import { maxWidth, maxWidthDetail } from "styles/mixin";
 
-export default function CommunitySection() {
+export default function DetailSection({ children }: any) {
   return (
-    <Container>
-      <DetailBanner />
-    </Container>
+    <DetailContainer>
+      <div className="detail-max-width">{children}</div>
+    </DetailContainer>
   );
 }
 
-const Container = styled.section`
-  display: flex;
-  max-width: 400px;
-  height: 400px;
-  margin: 0px auto;
-  .section-title {
-    ${fonts.H2};
-    padding-bottom: 8px;
-  }
-  .cards {
-    display: flex;
-    gap: 20px;
-    a {
-      flex: 1;
-    }
+const DetailContainer = styled.div`
+  ${maxWidth}
+  padding: 0px 0px 50px 0px;
+  .detail-max-width {
+    ${maxWidthDetail}
   }
 `;
