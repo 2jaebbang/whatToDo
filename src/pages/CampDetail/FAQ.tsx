0116bf0cce16cd5565e8450abaf79c9a977f2194
2jaebbang <2jaebbang@naver.com>
2022-02-12 21:19:02 +0900

    Question컴포넌트 생성 후 FAQ컴포넌트에 적용

--- src/pages/CampDetail/FAQ.tsx
+++ src/pages/CampDetail/FAQ.tsx
@@ -5,65 +5,22 @@ import fonts from "styles/fonts";
 import DetailSection from "./DetailSection";
 import toggleUp from "resources/images/faq-toggle-up.png";
 import toggleDown from "resources/images/faq-toggle-down.png";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import CampStore from "stores/CampStore";
+import Question from "./Question";
 
 export default function FAQ() {
-  const [toggleFAQ, setToggleFAQ] = useState<boolean>(false);
-  const onToggleFAQ = () => {
-    setToggleFAQ((prev) => !prev);
-  };
+  const campStore = useContext(CampStore);
 
   return (
     <Container>
       <DetailSection>
         <div className="faq-title">FAQ</div>
         <ul className="faq-list">
-          <Question isToggle={toggleFAQ} onClick={onToggleFAQ}>
-            <div className="faq-question">
-              <div>
-                <p>
-                  Q.<span>React 경험이 없는데 참여할 수 있나요?</span>
-                </p>
-              </div>
-              <div className="toggle-faq">
-                <img src={toggleFAQ ? toggleUp : toggleDown} />
-              </div>
-            </div>
-            <div className="faq-answer">
-              <p>
-                A.
-                <span>
-                  React 경험이 있는 분들이 협업 관점에서 업그레이드 할 수 있게
-                  도와드리는데 목적이 있는 클래스입니다. React 기초 강의는
-                  제공하지 않기 때문에 따로 React 공부 하실 수 있는 분들께
-                  추천드립니다.
-                </span>
-              </p>
-            </div>
-          </Question>
-          <Question isToggle={toggleFAQ} onClick={onToggleFAQ}>
-            <div className="faq-question">
-              <div>
-                <p>
-                  Q.<span>React 경험이 없는데 참여할 수 있나요?</span>
-                </p>
-              </div>
-              <div className="toggle-faq">
-                <img src={toggleFAQ ? toggleUp : toggleDown} />
-              </div>
-            </div>
-            <div className="faq-answer">
-              <p>
-                A.
-                <span>
-                  React 경험이 있는 분들이 협업 관점에서 업그레이드 할 수 있게
-                  도와드리는데 목적이 있는 클래스입니다. React 기초 강의는
-                  제공하지 않기 때문에 따로 React 공부 하실 수 있는 분들께
-                  추천드립니다.
-                </span>
-              </p>
-            </div>
-          </Question>
+          {campStore.targetCamp!.faqs.map((faq, index) => (
+            <Question key={index} question={faq.question} answer={faq.answer} />
+          ))}
         </ul>
       </DetailSection>
     </Container>
@@ -91,38 +48,5 @@ const Container = styled.div`
     flex-direction: column;
     align-self: center;
     gap: 10px;
-  }
-`;
-
-const Question = styled.li<{ isToggle: boolean }>`
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  padding: 20px 0px 20px 0px;
-  border-bottom: 1px solid ${colors.gray4};
-  cursor: pointer;
-
-  .faq-question {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    ${fonts.H4}
-  }
-  .faq-answer {
-    flex-direction: row;
-    ${fonts.Body2}
-    padding: 10px 0px 10px 0px;
-    display: ${(props) => (props.isToggle ? "flex" : "none")};
-  }
-  p {
-    padding-right: 5px;
-  }
-  .toggle-faq {
-    width: 18px;
-    img {
-      transform: ${(props) => (props.isToggle ? "rotate(180deg)" : "")};
-      transition: transform 0.25s cubic-bezier(0.4, 0, 0.2, 1);
-    }
   }
 `;
